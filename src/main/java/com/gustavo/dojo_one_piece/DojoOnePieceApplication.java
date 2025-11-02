ddc61cf2898b03bd011e4477be49cef2babe7a28
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2025-11-02 18:23:05 -0300

    first commit

---
+++ src/main/java/com/gustavo/dojo_one_piece/DojoOnePieceApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.dojo_one_piece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoOnePieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoOnePieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/dojo_one_piece/DojoOnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.dojo_one_piece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoOnePieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

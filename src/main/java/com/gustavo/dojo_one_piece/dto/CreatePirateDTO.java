8cd49574fa1dec267fb9f0a527f01c52dcaec4ef
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2025-11-05 17:37:21 -0300

    feature: creating CratePirateDTO.

---
+++ src/main/java/com/gustavo/dojo_one_piece/dto/CreatePirateDTO.java
@@ -0,0 +1,13 @@
+package com.gustavo.dojo_one_piece.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreatePirateDTO (
+
+    @NotBlank String name,
+    @NotNull String race,
+    @NotBlank String crew,
+    @NotNull String status
+
+) {}
